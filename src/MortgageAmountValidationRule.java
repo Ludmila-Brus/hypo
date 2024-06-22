9526d08b45017ad8fcab4a3b2e40fbfe8a03f422
Ludmila Brus <luntalumi@yandex.ru>
2024-06-23 00:55:32 +0300

    first

---
+++ src/MortgageAmountValidationRule.java
@@ -0,0 +1,14 @@
+// Дополните класс для проверки суммы ипотеки пользователя
+public class MortgageAmountValidationRule extends ValidationRule<Integer> {
+
+public MortgageAmountValidationRule(Integer value) {
+        super(value, "Минимальный размер ипотеки - 1.000.000, а максимальный - 10.000.000");
+        }
+
+        // Объявите и реализуйте метод для проверки суммы ипотеки ниже
+        @Override
+        public boolean isValid() {
+            return value >= 1000000 && value <= 10000000;
+        }
+
+        }
